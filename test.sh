31e223179b0db6fa9eb27e63d7ac56ddf4c3a14e
weezer <weezer.su@gmail.com>
2016-06-23 19:53:49 +0000

    add the remote and fix the run

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
-!/bin/bash
+#!/bin/sh
 
 
 sed -i "/dashboard_url/c\dashboard_url=$VART" horizon.conf
-sed -i "s/^username=.*/username=$USR/g" horizon.conf
-sed -i "s/^password=.*/password=$PWD/g" horizon.conf
-sed -i "s/^username='*admin_username=/admin_username=$ADUSR" horizon.conf
-sed -i "s/^username='*admin_password=/admin_password=$ADPWD" horizon.conf
-docker run -rm -t $os_release .
+sed -i "s/^username=.*/username=$VUSR/g" horizon.conf
+sed -i "s/^password=.*/password=$VPWD/g" horizon.conf
+sed -i "s/^admin_username=.*/admin_username=$ADUSR/g" horizon.conf
+sed -i "s/^admin_password=.*/admin_password=$ADPWD/g" horizon.conf
+echo $os_release
+docker run -t $os_release
+docker rm $(docker ps -a -q)
